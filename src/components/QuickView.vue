ba08c832c68466978b0d8f648d4caac8ce588802
Trong Binh <trongbinhnguyen15@gmail.com>
2022-01-22 20:43:38 +0700

    event listener

--- src/components/QuickView.vue
+++ src/components/QuickView.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>This is Quick View</div>
+  <div>This is Quick View</div>
+  <div>{{ selectedPokemon }}</div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+const selectedPokemon = ref("");
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+window.addEventListener("pokemon-selected", (e: any) => {
+  selectedPokemon.value = e.detail;
+});
+</script>
